c5f3a62e4de7e1f81411e5a05bf3cc4fa0aa42e0
Drew DeVault <sir@cmpwn.com>
2020-10-02 12:12:44 -0400

    Add example code

---
+++ examples/background.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+
+	"git.sr.ht/~sircmpwn/dowork"
+)
+
+func main() {
+	log.Println("Starting queue")
+	log.Println("SIGINT to terminate")
+	queue := work.NewQueue()
+	queue.Start(context.Background())
+
+	ntasks := 0
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+
+	go func() {
+		for {
+			reader := bufio.NewReader(os.Stdin)
+			log.Println("Enter time.ParseDuration string:")
+
+			input, _ := reader.ReadString('\n')
+			d, err := time.ParseDuration(strings.Trim(input, "\n"))
+			if err != nil {
+				log.Println("%v", err)
+				continue
+			}
+
+			ntasks += 1
+			queue.Submit(func(n int) func(context.Context) error {
+				return func(ctx context.Context) error {
+					log.Printf("Task %d started", n)
+					select {
+					case <-time.After(d):
+						break
+					case <-ctx.Done():
+						break
+					}
+					log.Printf("Task %d complete", n)
+					return nil
+				}
+			}(ntasks))
+		}
+	}()
+
+	<-sig
+	log.Println("Shutting down queue...")
+	queue.Shutdown()
+	log.Println("Shut down.")
+}
